0122f5484dbc15527d349d17010932259ea1027e
Vasiliy <vasiliyasazonov@gmail.com>
2022-09-24 12:24:15 +0300

    add function CreateSizeArray

--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿string[] row = { "dormer", "toy shop", "toy", "=)", "heater", "pot", "ray", "root", "air" };
+
+string[] CreateSizeArray(string[] row)
+{
+    int count = 0;
+    for (int i = 0; i < row.Length; i++)
+    {
+        if (row[i].Length <= 3)
+        {
+            count++;
+        }
+        else continue;
+    }
+    string[] array = new string[count];
+    return array;
+}
+
